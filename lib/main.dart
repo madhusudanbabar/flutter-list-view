901da35c821393ce6f51c5a1e89f59ccf7bc4802
Krypton <madhusudanbabar@gmail.com>
2020-12-13 20:10:37 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:course_app/app_screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "exploring UI widgets",
+    home: Home(),
+  ));
+}
